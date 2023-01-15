aee73d3513553dfbb083e4b546d44a641ccd043b
DavidKielty1 <78271797+DavidKielty1@users.noreply.github.com>
2023-01-15 11:27:28 +0000

    Custom hook useStore for global state. No redux/context

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
+import createRoot from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
-import ProductsProvider from "./context/products-context";
+import configureProductStore from "./hooks-store/products-store";
+
+configureProductStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
